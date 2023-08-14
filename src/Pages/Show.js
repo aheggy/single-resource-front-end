c368ff5519885615f547dc486386ab7827537a0c
Monir Hossen <monirhossen@pursuit.org>
2023-08-13 21:02:45 -0400

    Pages created

---
+++ src/Pages/Show.js
@@ -0,0 +1,14 @@
+import CitieDetails from "../components/CitieDetails";
+
+function Show() {
+  return (
+    <div>
+      <h2 className="m-5">City Details</h2>
+      <div>
+        <CitieDetails />
+      </div>
+    </div>
+  );
+}
+
+export default Show;
