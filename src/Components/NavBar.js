9cda101d515d4ea3f40802f190e49eb83b64a096
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-08-14 20:28:03 -0400

    chnging2 style of navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <nav>
-      <h1>
+      <h1 className="logo">
         <Link to="/bookmarks">TOP CITIES</Link>
       </h1>
       <button>
